65ce1010a940e7bcb04d189c2daf7ee031b042e8
dali <daki9981@qq.com>
2021-07-01 12:44:46 +0800

    配合eureka-server7001进行单机版服务搭建 进行eureka相关配置

--- cloud-provider-payment8001/src/main/java/com/youliao/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/youliao/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.youliao.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author Dali
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
