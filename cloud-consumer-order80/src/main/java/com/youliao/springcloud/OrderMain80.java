ac4d081181609bff79cd533df75ee49777533b8d
dali <daki9981@qq.com>
2021-07-01 12:44:33 +0800

    配合eureka-server7001进行单机版服务搭建 进行eureka相关配置

--- cloud-consumer-order80/src/main/java/com/youliao/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/youliao/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.youliao.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author Dali
@@ -10,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description
  */
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 //@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration=MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
