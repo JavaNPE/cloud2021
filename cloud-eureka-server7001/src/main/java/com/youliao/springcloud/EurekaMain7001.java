da3c099176aec577f7e8d5f86de278b5d9c1bd42
dali <daki9981@qq.com>
2021-07-01 12:42:54 +0800

    单机版Eureka服务搭建

---
+++ cloud-eureka-server7001/src/main/java/com/youliao/springcloud/EurekaMain7001.java
@@ -0,0 +1,20 @@
+package com.youliao.springcloud;
+
+import com.netflix.discovery.EurekaNamespace;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author Dali
+ * @Date 1/7/2021 上午 11:33
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
