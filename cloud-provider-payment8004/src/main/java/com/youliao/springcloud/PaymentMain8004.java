8d0cd26b2ead1468f6ca7f86167566632b725614
dali <daki9981@qq.com>
2021-07-02 19:26:42 +0800

    服务提供者：cloud-provider-payment8004 注册到zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/youliao/springcloud/PaymentMain8004.java
@@ -0,0 +1,20 @@
+package com.youliao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.expression.spel.SpelCompilerMode;
+
+/**
+ * @Author Dali
+ * @Date 2/7/2021 下午 2:04
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient //该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/youliao/springcloud/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.youliao.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @Author Dali
+ * @Date 2/7/2021 下午 2:10
+ * @Version 1.0
+ * @Description
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/zk")
+    public String paymentzk() {
+        return "springcloud with zookeeper:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
