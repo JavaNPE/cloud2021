cd7574fa24088385344dc1baf02300727a0a86e1
dali <daki9981@qq.com>
2021-07-03 00:24:33 +0800

    微服务消费者cloud-consumerconsul-order80服务注册进去Consul中

---
+++ cloud-consumerconsul-order80/src/main/java/com/youliao/springcloud/OrderConsulMain80.java
@@ -0,0 +1,19 @@
+package com.youliao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author Dali
+ * @Date 3/7/2021 上午 12:08
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient //该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class,args);
+    }
+}
