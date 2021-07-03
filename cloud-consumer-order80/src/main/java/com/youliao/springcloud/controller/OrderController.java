d44715f714b74f809d5d1244dab1dc3b208893b4
dali <daki9981@qq.com>
2021-07-03 15:20:13 +0800

    RestTemplate的getForObject方法和getForEntity方法的区别？ getForObject：返回对象为响应体中数据转化成的对象，基本可以理解为Json。 getForEntity：返回对象为ResponseEntity对象，包含了响应中的一些重要信息，比如响应头、响应状态码、响应体等

--- cloud-consumer-order80/src/main/java/com/youliao/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/youliao/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.youliao.springcloud.controller;
 import com.youliao.springcloud.entities.CommonResult;
 import com.youliao.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 public class OrderController {
-//    public static final String PAYMENT_URL = "http://localhost:8001";
+    //    public static final String PAYMENT_URL = "http://localhost:8001";
     public static final String PAYMENT_URL = "http://cloud-payment-service";
 
     @Resource
@@ -30,9 +31,31 @@ public class OrderController {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
+    /**
+     * getForObject：返回对象为响应体中数据转化成的对象，基本可以理解为Json。
+     *
+     * @param id
+     * @return
+     */
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/   " + id, CommonResult.class);
+    }
+
+    /**
+     * getForEntity:返回对象为ResponseEntity对象，包含了响应中的一些重要信息，比如响应头、响应状态码、响应体等
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/   " + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败");
+        }
     }
 }
 
