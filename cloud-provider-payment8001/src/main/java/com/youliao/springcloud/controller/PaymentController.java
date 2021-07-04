20c3afb204deb02fae62f7b5e02a16ca01864227
dali <daki9981@qq.com>
2021-07-04 19:08:10 +0800

    得到服务清单列表:一个微服务下面的全部具体实例

--- cloud-provider-payment8001/src/main/java/com/youliao/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/youliao/springcloud/controller/PaymentController.java
@@ -6,9 +6,12 @@ import com.youliao.springcloud.entities.Payment;
 import com.youliao.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * (Payment)表控制层
@@ -27,6 +30,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     /**
      * 通过主键查询单条数据
@@ -54,5 +60,24 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "没有对应记录，查询ID:" + id, null);
         }
+    }
+
+    /**
+     * 得到服务清单列表
+     *
+     * @return
+     */
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("*******element:" + element);
+        }
+        //一个微服务下面的全部具体实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
