e49fa7c8990744529c1aea500cbffacc6938dd27
dali <daki9981@qq.com>
2021-07-04 23:33:06 +0800

    自己写的负载均衡 轮询算法；80订单微服务改造 OrderController

--- cloud-consumer-order80/src/main/java/com/youliao/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/youliao/springcloud/controller/OrderController.java
@@ -1,8 +1,12 @@
 package com.youliao.springcloud.controller;
 
+
 import com.youliao.springcloud.entities.CommonResult;
 import com.youliao.springcloud.entities.Payment;
+import com.youliao.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 /**
  * @Author Dali
@@ -27,6 +33,12 @@ public class OrderController {
 
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
@@ -61,6 +73,22 @@ public class OrderController {
         } else {
             return new CommonResult<>(444, "操作失败");
         }
+    }
+
+    /**
+     * 自己写的负载均衡 轮询算法
+     *
+     * @return
+     */
+    @GetMapping(value = "/consumer/payment/lb")
+    public String getPaymentLB() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if (instances == null || instances.size() <= 0) {
+            return null;
+        }
+        ServiceInstance serviceInstance = loadBalancer.instance(instances);
+        URI uri = serviceInstance.getUri();
+        return restTemplate.getForObject(uri + "/payment/lb", String.class);
     }
 }
 
