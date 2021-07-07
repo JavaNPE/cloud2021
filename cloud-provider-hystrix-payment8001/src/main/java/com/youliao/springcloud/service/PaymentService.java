5a9bd0964020bf48c95989024e55ffcd4cb47b03
dali <daki9981@qq.com>
2021-07-07 16:31:22 +0800

    Hystrix之服务降级cloud-provider-hystrix-payment8001支付侧：fallback

--- cloud-provider-hystrix-payment8001/src/main/java/com/youliao/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/youliao/springcloud/service/PaymentService.java
@@ -24,18 +24,19 @@ public class PaymentService {
         return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_OK,id：  " + id + "\t" + "哈哈哈";
     }
 
-    //服务降级：3秒以内走正常的逻辑，（业务逻辑需要5秒）超过3秒，则出现超时的错误，我们走兜底方案：paymentInfo_TimeOutHandler
+    //服务降级：3秒以内走正常的逻辑，（业务逻辑需要5秒）超过3秒，则出现超时的错误（或者计算异常），我们走兜底方案：paymentInfo_TimeOutHandler
     @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")  //峰值：3秒
     })
     public String paymentInfo_TimeOut(Integer id) {
-        int timeNumber = 5; //业务逻辑需要5秒，
-        try {
+        //int timeNumber = 5; //业务逻辑需要5秒，超时异常
+        int age = 10 / 0;   //计算异常
+        /*try {
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id：  " + id + "\t" + "呜呜呜" + " 耗时(秒)" + timeNumber;
+        }*/
+        return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id：  " + id + "\t" + "呜呜呜" + " 耗时(秒)" /*+ timeNumber*/;
     }
 
     public String paymentInfo_TimeOutHandler(Integer id) {
