e50b941c01fa81f84fe6a7f9624411874adade47
dali <daki9981@qq.com>
2021-07-07 16:15:26 +0800

    设置自身调用超时时间的峰值，峰值内可以正常运行，超过了需要有兜底的方法处理，作服务降级fallback

--- cloud-provider-hystrix-payment8001/src/main/java/com/youliao/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/youliao/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.youliao.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -22,15 +24,23 @@ public class PaymentService {
         return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_OK,id：  " + id + "\t" + "哈哈哈";
     }
 
-    //失败
+    //服务降级：3秒以内走正常的逻辑，（业务逻辑需要5秒）超过3秒，则出现超时的错误，我们走兜底方案：paymentInfo_TimeOutHandler
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")  //峰值：3秒
+    })
     public String paymentInfo_TimeOut(Integer id) {
-        int timeNumber = 3;
+        int timeNumber = 5; //业务逻辑需要5秒，
         try {
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return "线程池：" + Thread.currentThread().getName() + "   paymentInfo_TimeOut,id：  " + id + "\t" + "呜呜呜" + " 耗时(秒)" + timeNumber;
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id) {
+        return "线程池：" + Thread.currentThread().getName() + "   系统繁忙或者运行报错,请稍后再试,id：  " + id + "\t" + "/(ㄒoㄒ)/~~";
+
     }
 
 }
