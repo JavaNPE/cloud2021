efe361881344e9e3a6e84a813bf34c0d18bf80d0
dali <daki9981@qq.com>
2021-07-07 15:42:57 +0800

    Hystrix案例:cloud-consumer-feign-hystrix-order80 Jmeter压测

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/youliao/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,21 @@
+package com.youliao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * @Author Dali
+ * @Date 2021/07/07 下午 12:42
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class, args);
+    }
+}
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/youliao/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,35 @@
+package com.youliao.springcloud.controller;
+
+import com.youliao.springcloud.service.PaymentHystrixService;
+import jdk.nashorn.internal.ir.IfNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author Dali
+ * @Date 2021/07/07 下午 12:55
+ * @Version 1.0
+ * @Description
+ */
+@RestController
+@Slf4j
+public class OrderHystrixController {
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        return result;
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        return result;
+    }
+}
