44239e319004f0583f8e480e7d5fd8f945c0d94e
dali <daki9981@qq.com>
2021-07-08 11:28:14 +0800

    可以在Controller处设置 @DefaultProperties(defaultFallback = "payment_Global_FallbackMethod") ；@HystrixCommand标签的使用，出现异常进行全局fallback方法。

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/youliao/springcloud/OrderHystrixMain80.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/youliao/springcloud/OrderHystrixMain80.java
@@ -3,6 +3,7 @@ package com.youliao.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
@@ -14,6 +15,7 @@ import org.springframework.cloud.openfeign.FeignClient;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
 public class OrderHystrixMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderHystrixMain80.class, args);
--- cloud-consumer-feign-hystrix-order80/src/main/java/com/youliao/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/youliao/springcloud/controller/OrderHystrixController.java
@@ -1,7 +1,9 @@
 package com.youliao.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.youliao.springcloud.service.PaymentHystrixService;
-import jdk.nashorn.internal.ir.IfNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,19 +19,35 @@ import javax.annotation.Resource;
  */
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")  //全局的
 public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
 
     @GetMapping("/consumer/payment/hystrix/ok/{id}")
-    public String paymentInfo_OK(@PathVariable("id") Integer id){
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.paymentInfo_OK(id);
         return result;
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {    //找特定的兜底方法
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")  //1.5秒钟以内就是正常的业务逻辑
+//    })//峰值：1.5秒 如果超过1.5秒 采用我们兜底的方法：paymentTimeOutFallbackMethod，如果没有超过走我们正常的逻辑
+    @HystrixCommand //此处依旧照写@HystrixCommand注解标签，不写的话表示不进行服务降级，反之出现异常进行全局兜底处理
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        int age = 10/0;
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
+    }
+
+    //兜底方法
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+        return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
+    }
+
+    //下面是全局fallback方法
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息，请稍后再试,(┬＿┬)";
     }
 }
