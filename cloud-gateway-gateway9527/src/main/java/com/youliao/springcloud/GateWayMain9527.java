b3f0859bf7cb869b526231e9bdc1df8218a5b6b1
dali <daki9981@qq.com>
2021-07-11 15:51:18 +0800

    Gateway入门配置：Gateway9527Module搭建

---
+++ cloud-gateway-gateway9527/src/main/java/com/youliao/springcloud/GateWayMain9527.java
@@ -0,0 +1,21 @@
+package com.youliao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author Dali
+ * @Date 2021/07/11 下午 2:56
+ * @Version 1.0
+ * @Description
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
+
