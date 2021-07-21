8dc9921e8d787dcfd8b49408714f0c67f2f04616
dali <daki9981@qq.com>
2021-07-21 12:20:23 +0800

    SpringCloud Config客户端配置与测试

---
+++ cloud-config-client-3355/src/main/java/com/youliao/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,20 @@
+package com.youliao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author Dali
+ * @Date 2021/07/20 下午 11:13
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
+
---
+++ cloud-config-client-3355/src/main/java/com/youliao/springcloud/Controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.youliao.springcloud.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Dali
+ * @Date 2021/07/20 下午 11:16
+ * @Version 1.0
+ * @Description
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
+
