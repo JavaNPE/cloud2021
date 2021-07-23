26551c5080692f1ddfeee2330342e50995f67bcb
dali <daki9981@qq.com>
2021-07-23 11:01:22 +0800

    SpringCloud Stream消息驱动:分组消费与持久化,依照8802，clone出来一份运行8803,目前是8802/8803同时都收到了，存在重复消费问题

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/youliao/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.youliao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Dali
+ * @Date 2021/07/23 上午 10:35
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
