238f3f1b72dc72e852d3e8fb355713f58b8061fd
dali <daki9981@qq.com>
2021-07-23 13:42:09 +0800

    分组消费:8802/8803都变成相同组，group两个相同(group: atguiguA)

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/youliao/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.youliao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Dali
+ * @Date 2021/07/22 下午 10:52
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
