c063021a671beb523fe81f097f81384de04c5e7e
dali <daki9981@qq.com>
2021-07-24 12:24:41 +0800

    Nacos作为服务注册中心演示（替代Eureka）:为了下一章节演示nacos的负载均衡，参照9001新建9002

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/youliao/springcloud/PaymentMain9002.java
@@ -0,0 +1,19 @@
+package com.youliao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author Dali
+ * @Date 2021/07/24 下午 12:11
+ * @Version 1.0
+ * @Description
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9002.class, args);
+    }
+}
