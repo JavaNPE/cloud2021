d64147ff8c59ac38bfe380d4d541a75477ed2d22
dali <daki9981@qq.com>
2021-07-29 15:36:34 +0800

    SpringCloud Alibaba Sentinel实现熔断与限流:流控规则,排队等待

--- cloudalibaba-sentinel-service8401/src/main/java/com/youliao/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/youliao/springcloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.youliao.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author Dali
@@ -11,14 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@Slf4j
 public class FlowLimitController {
     @GetMapping("/testA")
     public String testA() {
+       /* try { //暂停0.8毫秒
+            TimeUnit.MILLISECONDS.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
         return "------testA";
     }
 
     @GetMapping("/testB")
     public String testB() {
+        log.info(Thread.currentThread().getName()+"\t"+"...testB");
         return "------testB";
     }
 }
